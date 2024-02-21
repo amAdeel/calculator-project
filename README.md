d073b66c78680ec24d4caf41a5535037d20fe8b6
amAdeel <adeeleducationpurpose@gmail.com>
2024-02-21 09:31:28 +0500

    1st commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Calculator Package
+
+This is a simple calculator package implemented in TypeScript, with CLI (Command Line Interface) support.
+
+## Installation
+
+To install the calculator package, you can use npm. Run the following command:
+
+```bash
+npm install my-calculator
+
+Usage
+You can use the calculator package by importing it into your TypeScript file and calling the appropriate functions. Here's an example:
+
+import { welcome } from 'iamadeel-calculator';
+
+welcome(); // This will prompt the user to enter numbers and an operator, and then perform the calculation.
+
+Features
+Addition
+Subtraction
+Multiplication
+Division
+Exponentiation
+Example
+Here's an example of how you can use the calculator package in your TypeScript file:
+
+import { sum, subtract, multiply, division, power } from 'iamadeel-calculator';
+
+sum(5, 3); // Output: 8
+subtract(7, 2); // Output: 5
+multiply(4, 6); // Output: 24
+division(10, 2); // Output: 5
+power(2, 3); // Output: 8
+
+Contributing
+Contributions are welcome! If you'd like to contribute to this project, please fork the repository and submit a pull request.
+
+License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+Author
+[i am Adeel]
---
+++ prac.js
@@ -0,0 +1,94 @@
+// ========================> use of inquirer <============================
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import chalkanimation from 'chalk-animation';
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    });
+};
+async function hyeverone() {
+    let animationTitle = chalkanimation.rainbow('I am a calculator');
+    console.log(chalk.greenBright(`
+    _____________________
+   |  _________________  |
+   | |      CALC       | |
+   | |_________________| |
+   |  ___ ___ ___   ___  |
+   | | 7 | 8 | 9 | | + | |
+   | |___|___|___| |___| |
+   | | 4 | 5 | 6 | | - | |
+   | |___|___|___| |___| |
+   | | 1 | 2 | 3 | | x | |
+   | |___|___|___| |___| |
+   | | . | 0 | = | | / | |
+   | |___|___|___| |___| |
+   |_____________________|
+   `));
+    animationTitle.start();
+    await sleep();
+    animationTitle.stop();
+}
+hyeverone();
+function welcome() {
+    const response = inquirer.prompt([{
+            type: "number",
+            name: "num1",
+            message: "Enter the first Num",
+        },
+        {
+            type: "number",
+            name: "num2",
+            message: "Enter the Second Num"
+        },
+        {
+            type: "list",
+            name: "operator",
+            choices: ["+", "-", "*", "/", "^"],
+            message: "Enter the operator"
+        }
+    ])
+        .then((answers) => {
+        console.log(answers);
+        if (answers.operator === '+') {
+            sum(answers.num1, answers.num2);
+        }
+        else if (answers.operator === '-') {
+            subtract(answers.num1, answers.num2);
+        }
+        else if (answers.operator === '*') {
+            multiply(answers.num1, answers.num2);
+        }
+        else if (answers.operator === '/') {
+            division(answers.num1, answers.num2);
+        }
+        else if (answers.operator === '^') {
+            power(answers.num1, answers.num2);
+        }
+        else
+            (console.log('invalid operator'));
+    });
+}
+function sum(num1, num2) {
+    const result = num1 + num2;
+    console.log('sum:', result);
+}
+function subtract(num1, num2) {
+    const result = num1 - num2;
+    console.log('subtract:', result);
+}
+function multiply(num1, num2) {
+    const result = num1 * num2;
+    console.log('Multiplay:', result);
+}
+function division(num1, num2) {
+    const result = num1 / num2;
+    console.log('division:', result.toFixed(2));
+}
+function power(num1, num2) {
+    const result = num1 ** num2;
+    console.log('power:', result);
+}
+setTimeout(() => {
+    welcome();
+}, 2000);
